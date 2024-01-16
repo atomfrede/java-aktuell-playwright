8c01880786dab22d46b7ec684531bdc717ee03e6
Frederik Hahne <atomfrede@gmail.com>
2024-01-16 22:25:04 +0100

    next chapter

--- src/test/java/com/github/atomfrede/playwrightjavaaktuell/PlaywrightOhneTestRunner.java
+++ src/test/java/com/github/atomfrede/playwrightjavaaktuell/PlaywrightOhneTestRunner.java
@@ -11,7 +11,9 @@ import static com.microsoft.playwright.assertions.PlaywrightAssertions.assertTha
 public class PlaywrightOhneTestRunner {
 
     public static void main(String[] args) {
-        try (Playwright playwright = Playwright.create(); Browser browser = playwright.firefox().launch(new BrowserType.LaunchOptions().setHeadless(true))) {
+        try (Playwright playwright = Playwright.create();
+             Browser browser = playwright.firefox()
+                     .launch(new BrowserType.LaunchOptions().setHeadless(true))) {
             BrowserContext context = browser.newContext();
 
             context.tracing().start(new Tracing.StartOptions()
@@ -23,11 +25,17 @@ public class PlaywrightOhneTestRunner {
 
             page.navigate("https://www.ijug.eu/de/home/");
 
-            assertThat(page).hasTitle(Pattern.compile("Home: iJUG"));
+            assertThat(page).hasTitle("Home: iJUG");
 
             page.locator("#nav-primary").getByRole(AriaRole.LINK, new Locator.GetByRoleOptions().setName("Java aktuell")).click();
-
             assertThat(page).hasTitle("Java aktuell: iJUG");
+
+            page.getByRole(AriaRole.LINK, new Page.GetByRoleOptions().setName("Weitere Informationen zum Magazin")).click();
+            assertThat(page).hasTitle("Zeitschrift: iJUG");
+
+            page.locator(".article.news-badge").first().screenshot(new Locator.ScreenshotOptions().setPath(Paths.get("build", "aktuelle-ausgabe.png")));
+            String header = page.locator(".news-badge__content h3").first().innerText();
+            System.out.println(header.split(":")[1].trim());
 
             context.tracing().stop(new Tracing.StopOptions()
                     .setPath(Paths.get("build/ohneRunner/trace.zip")));
