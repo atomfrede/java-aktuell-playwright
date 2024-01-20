cb05fdf638348410a912348c3e475de8f4e8a4f4
Frederik Hahne <atomfrede@gmail.com>
2024-01-20 22:25:38 +0100

    update article

--- src/test/java/com/github/atomfrede/playwrightjavaaktuell/PlaywrightOhneTestRunner.java
+++ src/test/java/com/github/atomfrede/playwrightjavaaktuell/PlaywrightOhneTestRunner.java
@@ -4,7 +4,6 @@ import com.microsoft.playwright.*;
 import com.microsoft.playwright.options.AriaRole;
 
 import java.nio.file.Paths;
-import java.util.regex.Pattern;
 
 import static com.microsoft.playwright.assertions.PlaywrightAssertions.assertThat;
 
